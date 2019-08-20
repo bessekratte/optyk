3ff8e066bb46fdaf037056ff716b1da24df11698
bessekratte <mateuszkierznowski@gmail.com>
2019-08-20 18:05:45 +0200

    add event listener

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,8 +2,15 @@ let pesel = document.getElementById("PESEL");
 let zipCode = document.getElementById("zip");
 
 pesel.addEventListener("focusout", validatePESEL);
-zipCode.addEventListener("focusout", validateZip);
+pesel.addEventListener("focusin", test);
+// zipCode.addEventListener("onfocus", test);
 
+function removeSelection() {
+}
+
+function test() {
+    pesel.getS
+}
 
 function validatePESEL() {
     if (pesel.value.length != 11) {
@@ -27,5 +34,21 @@ function validateGlass(glass) {
     var regex = new RegExp("e");
     var regex = new RegExp("e");
     var isValid = regex.test(glass);
-
 }
+
+jQuery.extend(jQuery.expr[':'], {
+    focusable: function (el, index, selector) {
+        return $(el).is('a, button, :input, [tabindex]');
+    }
+});
+
+$(document).on('keydown', ':focusable', function (e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        // Get all focusable elements on the page
+        var $canfocus = $(':focusable');
+        var index = $canfocus.index(this) + 1;
+        if (index >= $canfocus.length) index = 0;
+        $canfocus.eq(index).focus();
+    }
+});
